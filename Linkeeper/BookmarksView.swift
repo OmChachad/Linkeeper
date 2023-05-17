7e2e5394335be32279aa22e77175897e7c9ef055
iTechEverything <96822675+iTechEverything@users.noreply.github.com>
2023-05-17 13:13:00 +0530

    Added "Group By Folders" Option

--- Linkeeper/BookmarksView.swift
+++ Linkeeper/BookmarksView.swift
@@ -12,6 +12,7 @@ struct BookmarksView: View {
     @Environment(\.keyboardShortcut) var keyboardShortcut
     
     @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Bookmark.date, ascending: true)]) var bookmarks: FetchedResults<Bookmark>
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Folder.index, ascending: true)]) var folders: FetchedResults<Folder>
     
     var folder: Folder?
     var favorites: Bool?
@@ -31,12 +32,25 @@ struct BookmarksView: View {
     @State private var deleteConfirmation = false
     @State private var movingBookmarks = false
     
+    @AppStorage("GroupAllByFolders") var groupByFolders: Bool = true
+    
     var minimumItemWidth: CGFloat {
         if UIScreen.main.bounds.width == 320 {
             return 145
         } else {
             return 165
         }
+    }
+    
+    init(folder: Folder?, onlyFavorites: Bool) {
+        if let folder = folder {
+            _bookmarks = FetchRequest<Bookmark>(sortDescriptors: [NSSortDescriptor(keyPath: \Bookmark.date, ascending: true)], predicate: NSPredicate(format: "folder == %@", folder))
+        } else if onlyFavorites {
+            _bookmarks = FetchRequest<Bookmark>(sortDescriptors: [NSSortDescriptor(keyPath: \Bookmark.date, ascending: true)], predicate: NSPredicate(format: "isFavorited == true"))
+        }
+        
+        self.folder = folder
+        self.favorites = onlyFavorites
     }
     
     var body: some View {
@@ -56,19 +70,56 @@ struct BookmarksView: View {
                 }
             } else {
                 ScrollView {
-                    if !searchText.isEmpty && filteredBookmarks.count == 0 {
-                        Text("No results found for \"\(searchText)\"")
-                            .foregroundColor(.secondary)
-                            .padding()
-                    }
-                    
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: minimumItemWidth))], spacing: 15) {
-                        ForEach(filteredBookmarks, id: \.self) { bookmark in
-                            BookmarkItem(bookmark: bookmark, namespace: nm, showDetails: $showDetails, toBeEditedBookmark: $toBeEditedBookmark, detailViewImage: $detailViewImage, selectedBookmarks: $selectedBookmarks)
-                                .padding(.horizontal, 5)
+                    Group {
+                        if !searchText.isEmpty && filteredBookmarks.isEmpty {
+                            Text("No results found for **\"\(searchText)\"**")
+                                .foregroundColor(.secondary)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                        }
+                        
+                        if groupByFolders && folder == nil {
+                            BookmarksGrid(for: ungroupedBookmarks)
+                                .padding(.horizontal, 15)
+                                .padding(.bottom , 2.5)
+                            
+                            ForEach(folders, id: \.self) { folder in
+                                let folderHasBookmarks = !folder.bookmarksArray.isEmpty
+                                if searchText.isEmpty || !filteredBookmarks(for: folder).isEmpty {
+                                    DisclosureGroup {
+                                        if folderHasBookmarks {
+                                            BookmarksGrid(for: filteredBookmarks(for: folder))
+                                                .padding(.vertical, 5)
+                                        } else {
+                                            Text("No Bookmarks")
+                                                .foregroundColor(.secondary)
+                                                .frame(maxWidth: .infinity, minHeight: 80, idealHeight: 100, maxHeight: 100)
+                                                .background(.regularMaterial)
+                                                .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
+                                                .padding(.horizontal, 5)
+                                        }
+                                    } label: {
+                                        Label {
+                                            Text(folder.wrappedTitle)
+                                                .foregroundColor(.primary)
+                                        } icon: {
+                                            Image(systemName: folder.wrappedSymbol)
+                                                .imageScale(.large)
+                                                .foregroundColor(folder.wrappedColor)
+                                        }
+                                        .padding(.vertical, 5)
+                                    }
+                                    .expandByDefault(folderHasBookmarks)
+                                    .padding(.horizontal, 15)
+                                    .padding(.vertical, 5)
+                                }
+                            }
+                        } else {
+                            BookmarksGrid(for: filteredBookmarks)
+                                .padding(.horizontal, 10)
                         }
                     }
-                    .padding(.horizontal, 10)
+                    .frame(maxWidth: .infinity)
                 }
                 .searchable(text: $searchText, prompt: "Find a bookmark...")
             }
@@ -91,6 +142,10 @@ struct BookmarksView: View {
             if editState == .inactive {
                 Menu {
                     Button { editState = .active } label: { Label("Select", systemImage: "checkmark.circle") }
+                    
+                    if folder == nil {
+                        Toggle(isOn: $groupByFolders.animation(), label: { Label("Group by Folders", systemImage: "rectangle.grid.1x2") })
+                    }
                     
                     Button { addingBookmark.toggle() } label: { Label("Add Bookmark", systemImage: "plus") }
                         .keyboardShortcut("n", modifiers: .command)
@@ -156,21 +211,44 @@ struct BookmarksView: View {
         .animation(.easeInOut.speed(0.5), value: editState)
     }
     
-    init(folder: Folder?, onlyFavorites: Bool) {
-        if let folder = folder {
-            _bookmarks = FetchRequest<Bookmark>(sortDescriptors: [NSSortDescriptor(keyPath: \Bookmark.date, ascending: true)], predicate: NSPredicate(format: "folder == %@", folder))
-        } else if onlyFavorites {
-            _bookmarks = FetchRequest<Bookmark>(sortDescriptors: [NSSortDescriptor(keyPath: \Bookmark.date, ascending: true)], predicate: NSPredicate(format: "isFavorited == true"))
+    func BookmarksGrid(for bookmarks: [Bookmark]) -> some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: minimumItemWidth))], spacing: 15) {
+            ForEach(bookmarks, id: \.self) { bookmark in
+                BookmarkItem(bookmark: bookmark, namespace: nm, showDetails: $showDetails, toBeEditedBookmark: $toBeEditedBookmark, detailViewImage: $detailViewImage, selectedBookmarks: $selectedBookmarks)
+                    .padding(.horizontal, 5)
+            }
         }
-        self.folder = folder
-        self.favorites = onlyFavorites
     }
     
     var filteredBookmarks: [Bookmark] {
         if searchText.isEmpty {
             return [Bookmark](bookmarks)
         } else {
-            return bookmarks.filter { $0.wrappedTitle.localizedCaseInsensitiveContains(searchText) || $0.wrappedNotes.localizedCaseInsensitiveContains(searchText) || $0.wrappedHost.localizedCaseInsensitiveContains(searchText) }
+            return bookmarks.filter {
+                if let folder = $0.folder {
+                    return ($0.wrappedTitle + $0.wrappedHost + $0.wrappedNotes + folder.wrappedTitle).localizedCaseInsensitiveContains(searchText)
+                } else {
+                    return ($0.wrappedTitle + $0.wrappedHost + $0.wrappedNotes).localizedCaseInsensitiveContains(searchText)
+                }
+            }
+        }
+    }
+    
+    var ungroupedBookmarks: [Bookmark] {
+        let ungroupedBookmarks = bookmarks.filter({$0.folder == nil})
+        
+        if searchText.isEmpty {
+            return ungroupedBookmarks
+        } else {
+            return ungroupedBookmarks.filter { ($0.wrappedTitle + $0.wrappedHost + $0.wrappedNotes).localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
+    func filteredBookmarks(for folder: Folder) -> [Bookmark] {
+        if searchText.isEmpty {
+            return bookmarks.filter({$0.folder == folder})
+        } else {
+            return bookmarks.filter { $0.folder == folder && ($0.wrappedTitle + $0.wrappedHost + $0.wrappedNotes + $0.folder!.wrappedTitle).localizedCaseInsensitiveContains(searchText) }
         }
     }
     
---
+++ Linkeeper/Helpers/DisclosureGroup Style.swift
@@ -0,0 +1,45 @@
+//
+//  DisclosureGroup Style.swift
+//  Linkeeper
+//
+//  Created by Om Chachad on 17/05/23.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 16.0, *)
+struct ExpandedByDefault: DisclosureGroupStyle {
+    var expandByDefault: Bool
+    
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+            HStack {
+                configuration.label
+                Spacer()
+                Image(systemName: "chevron.right")
+                    .imageScale(.small)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.accentColor)
+                    .rotationEffect(.degrees(configuration.isExpanded ? 90 : 0))
+            }
+            .contentShape(Rectangle())
+            .onAppear {
+                // To expand by default
+                if expandByDefault {
+                    configuration.isExpanded = true
+                }
+            }
+            .onTapGesture {
+                withAnimation(.default.speed(1.5)) {
+                    configuration.isExpanded.toggle()
+                }
+            }
+            if configuration.isExpanded {
+                configuration.content
+                    .disclosureGroupStyle(self)
+                    .transition(.opacity)
+            }
+        }
+    }
+}
