b824275d9799ecf65a9a9c9d014a69f8d0eed900
iTechEverything <96822675+iTechEverything@users.noreply.github.com>
2023-07-02 23:47:34 +0530

    Drop Destinations now return false upon fail

--- Linkeeper/Helpers/Drag & Dropping/DropExtension.swift
+++ Linkeeper/Helpers/Drag & Dropping/DropExtension.swift
@@ -14,33 +14,43 @@ extension View {
             if #available(iOS 16.0, *) {
                 self
                     .dropDestination(for: DropItem.self) { items, _ in
+                        var successStatus = true
+                        
                         items.forEach { droppedItem in
                             if let droppedBookmark = droppedItem.bookmark {
-                                print("DropAction: Dropping Bookmark")
                                 dropAction(droppedBookmark.bookmark, droppedBookmark.url)
                             } else if let url = droppedItem.url {
                                 if BookmarksManager.shared.getAllBookmarks().first(where: {$0.url == url.absoluteString}) == nil {
-                                    print("DropAction: Dropping URL")
                                     dropAction(nil, url)
+                                } else {
+                                    successStatus = false
                                 }
+                            } else {
+                                successStatus = false
                             }
                         }
-                        
-                        return true
+                        return successStatus
                     }
             } else {
                 self
                     .onDrop(of: ["public.url"], isTargeted: nil) { providers in
+                        var successStatus = true
+                        
                         providers.forEach { provider in
+                            
                             _ = provider.loadObject(ofClass: URL.self) { url, _ in
                                 if let url {
                                     if BookmarksManager.shared.getAllBookmarks().first(where: {$0.url == url.absoluteString}) == nil {
                                         dropAction(nil, url)
+                                    } else {
+                                        successStatus = false
                                     }
+                                } else {
+                                    successStatus = false
                                 }
                             }
                         }
-                        return true
+                        return successStatus
                     }
             }
         }
