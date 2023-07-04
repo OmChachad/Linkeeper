d1415e5362448fd0199e11226f8c10c2abf02311
iTechEverything <96822675+iTechEverything@users.noreply.github.com>
2023-07-04 23:40:09 +0530

    Added Enums Folder, and Organised .Swift files

--- Linkeeper/Helpers/Enums/SymbolOption.swift
+++ Linkeeper/Helpers/Enums/SymbolOption.swift
@@ -1,46 +1,11 @@
 //
-//  ColorsAndSymbols.swift
-//  Marked
+//  SymbolOption.swift
+//  Linkeeper
 //
-//  Created by Om Chachad on 27/04/22.
+//  Created by Om Chachad on 04/07/23.
 //
 
 import Foundation
-import SwiftUI
-
-enum ColorOption: String, CaseIterable {
-    case gray
-    case purple
-    case brown
-    case indigo
-    case pink
-    case blurple
-    case orange
-    case blue
-    case yellow
-    case cyan
-    case green
-    case mint
-    
-    private static var values: [ColorOption : Color] = [
-        .gray : Color(uiColor: .systemGray),
-        .purple : Color.purple,
-        .orange : Color.orange,
-        .pink : Color.red,
-        .yellow : Color.yellow,
-        .mint : Color.mint,
-        .indigo : Color.indigo,
-        .green : Color.green,
-        .cyan : Color.cyan,
-        .brown : Color.brown,
-        .blue : Color.blue,
-        .blurple : Color(red: 0.5294117647, green: 0.4823529412, blue: 0.9019607843)
-    ]
-    
-    var color: Color {
-        return ColorOption.values[self] ?? .gray
-    }
-}
 
 enum SymbolCategory: String, CaseIterable {
     case objects = "Objects"
