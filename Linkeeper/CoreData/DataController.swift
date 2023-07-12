36c7651bf19f884b7eda1039c737bb240926db30
iTechEverything <96822675+iTechEverything@users.noreply.github.com>
2023-07-12 23:33:59 +0530

    CoreData is now shared between App Extensions
    If a user has their Core Data in the old location, after running this version of Linkeeper, their database will be migrated to the App Group, and the user can no longer go back to previous versions of Linkeeper without losing their data.
    (However, users can re-update to the latest version to regain access to the data, provided [presumably] they did not uninstall the app or clear out its documents.)

--- Linkeeper/CoreData/DataController.swift
+++ Linkeeper/CoreData/DataController.swift
@@ -15,18 +15,63 @@ class DataController: ObservableObject {
         
     init() {
         persistentCloudKitContainer = NSPersistentCloudKitContainer(name: "Linkeeper")
+        
+        var oldStoreURL: URL?
+        if let storeDescription = persistentCloudKitContainer.persistentStoreDescriptions.first, let url = storeDescription.url {
+            oldStoreURL = FileManager.default.fileExists(atPath: url.path) ? url : nil
+        }
+        
+        let sharedStoreURL = URL.storeURL(for: "group.starlightapps.linkeeper", databaseName: "Linkeeper")
+        if oldStoreURL == nil {
+            let storeDescription = NSPersistentStoreDescription(url: sharedStoreURL)
+            persistentCloudKitContainer.persistentStoreDescriptions = [storeDescription]
+        }
+        
         guard let description = persistentCloudKitContainer.persistentStoreDescriptions.first else {
             fatalError ("Failed to initialize persistent container")
         }
         description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
+        description.cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.org.starlightapps.linkeeper")
         persistentCloudKitContainer.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         persistentCloudKitContainer.viewContext.automaticallyMergesChangesFromParent = true
         
-        persistentCloudKitContainer.loadPersistentStores { _, error in
+        persistentCloudKitContainer.loadPersistentStores { storeDescription, error in
             if let error = error {
-                print("Core Data failed to load: \(error.self)")
+                fatalError("Core Data failed to load: \(error.self)")
+            }
+            
+            // Moving old Core Data Store to App Group (from: https://stackoverflow.com/a/57020353)
+            if let oldStoreURL, oldStoreURL.absoluteString != sharedStoreURL.absoluteString {
+                let coordinator = self.persistentCloudKitContainer.persistentStoreCoordinator
+                if let oldStore = coordinator.persistentStore(for: oldStoreURL) {
+                    do {
+                        try coordinator.migratePersistentStore(oldStore, to: sharedStoreURL, options: nil, withType: NSSQLiteStoreType)
+                        
+                        // Code to delete the previous persistent store (from: https://stackoverflow.com/a/72585271)
+                        try coordinator.destroyPersistentStore(at: oldStoreURL, ofType: NSSQLiteStoreType, options: nil)
+                        NSFileCoordinator(filePresenter: nil).coordinate(writingItemAt: oldStoreURL.deletingLastPathComponent(), options: .forDeleting, error: nil, byAccessor: { url in
+                            try? FileManager.default.removeItem(at: url)
+                            try? FileManager.default.removeItem(at: url.deletingLastPathComponent().appendingPathComponent("\(self.persistentCloudKitContainer.name).sqlite-shm"))
+                            try? FileManager.default.removeItem(at: url.deletingLastPathComponent().appendingPathComponent("\(self.persistentCloudKitContainer.name).sqlite-wal"))
+                            try? FileManager.default.removeItem(at: url.deletingLastPathComponent().appendingPathComponent("ckAssetFiles"))
+                        })
+                    } catch {
+                        print(error.localizedDescription)
+                    }
+                    
+//                    // delete old store
+//                    let fileCoordinator = NSFileCoordinator(filePresenter: nil)
+//                    fileCoordinator.coordinate(writingItemAt: url, options: .forDeleting, error: nil, byAccessor: { url in
+//                        do {
+//                            try FileManager.default.removeItem(at: url)
+//                        } catch {
+//                            print(error.localizedDescription)
+//                        }
+//                    })
+                }
             }
         }
+        print(persistentCloudKitContainer.persistentStoreDescriptions)
     }
     
     func save(context: NSManagedObjectContext) {
---
+++ Linkeeper/CoreData/StoreURL Extension.swift
@@ -0,0 +1,20 @@
+//
+//  StoreURL Extension.swift
+//  Linkeeper
+//
+//  Created by Om Chachad on 11/07/23.
+//
+
+import Foundation
+
+public extension URL {
+
+    /// Returns a URL for the given app group and database pointing to the sqlite database.
+    static func storeURL(for appGroup: String, databaseName: String) -> URL {
+        guard let fileContainer = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup) else {
+            fatalError("Shared file container could not be created.")
+        }
+
+        return fileContainer.appendingPathComponent("\(databaseName).sqlite")
+    }
+}
