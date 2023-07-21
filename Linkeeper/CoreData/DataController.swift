5d608b0d48a4493dc8ed5f68c8d2876adc0052aa
iTechEverything <96822675+iTechEverything@users.noreply.github.com>
2023-07-21 10:55:10 +0530

    Refactored Core Data Initialisation

--- Linkeeper/CoreData/DataController.swift
+++ Linkeeper/CoreData/DataController.swift
@@ -1,141 +1,98 @@
-//
-//  DataController.swift
-//  Linkeeper
-//
-//  Created by Om Chachad on 25/05/22.
-//
-
 import CoreData
 import Foundation
 
 class DataController: ObservableObject {
     static let shared = DataController()
-    
+
     let persistentCloudKitContainer: NSPersistentCloudKitContainer
-        
-    init() {
+
+    private init() {
         persistentCloudKitContainer = NSPersistentCloudKitContainer(name: "Linkeeper")
-        
-        var oldStoreURL: URL?
-        if let storeDescription = persistentCloudKitContainer.persistentStoreDescriptions.first, let url = storeDescription.url {
-            oldStoreURL = FileManager.default.fileExists(atPath: url.path) ? url : nil
-        }
-        
+
         let sharedStoreURL = URL.storeURL(for: "group.starlightapps.linkeeper", databaseName: "Linkeeper")
-        if oldStoreURL == nil {
-            let storeDescription = NSPersistentStoreDescription(url: sharedStoreURL)
-            persistentCloudKitContainer.persistentStoreDescriptions = [storeDescription]
+
+        if let oldStoreURL = persistentCloudKitContainer.persistentStoreDescriptions.first?.url, oldStoreURL.absoluteString != sharedStoreURL.absoluteString {
+            migrateStore(from: oldStoreURL, to: sharedStoreURL)
         }
         
+        setupPersistentStore(with: sharedStoreURL)
+        configureContainer()
+        deduplicateBookmarksAndFolders()
+    }
+
+    private func setupPersistentStore(with storeURL: URL) {
+        let storeDescription = NSPersistentStoreDescription(url: storeURL)
+        persistentCloudKitContainer.persistentStoreDescriptions = [storeDescription]
+    }
+
+    private func configureContainer() {
         guard let description = persistentCloudKitContainer.persistentStoreDescriptions.first else {
-            fatalError ("Failed to initialize persistent container")
+            fatalError("Failed to initialize persistent container")
         }
+
         description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
         description.cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.org.starlightapps.Linkeeper")
+
         persistentCloudKitContainer.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
         persistentCloudKitContainer.viewContext.automaticallyMergesChangesFromParent = true
-        persistentCloudKitContainer.loadPersistentStores { storeDescription, error in
+
+        persistentCloudKitContainer.loadPersistentStores(completionHandler: { _, error in
             if let error = error {
-                fatalError("Core Data failed to load: \(error.self)")
+                fatalError("Core Data failed to load: \(error)")
             }
-            
-            // Moving old Core Data Store to App Group (from: https://stackoverflow.com/a/57020353)
-            if let oldStoreURL, oldStoreURL.absoluteString != sharedStoreURL.absoluteString {
-                let coordinator = self.persistentCloudKitContainer.persistentStoreCoordinator
-                if let oldStore = coordinator.persistentStore(for: oldStoreURL) {
-                    do {
-                        try coordinator.migratePersistentStore(oldStore, to: sharedStoreURL, options: nil, withType: NSSQLiteStoreType)
-                        
-                        // Code to delete the previous persistent store (from: https://stackoverflow.com/a/72585271)
-                        try coordinator.destroyPersistentStore(at: oldStoreURL, ofType: NSSQLiteStoreType, options: nil)
-                        NSFileCoordinator(filePresenter: nil).coordinate(writingItemAt: oldStoreURL.deletingLastPathComponent(), options: .forDeleting, error: nil, byAccessor: { url in
-                            try? FileManager.default.removeItem(at: url)
-                            try? FileManager.default.removeItem(at: url.deletingLastPathComponent().appendingPathComponent("\(self.persistentCloudKitContainer.name).sqlite-shm"))
-                            try? FileManager.default.removeItem(at: url.deletingLastPathComponent().appendingPathComponent("\(self.persistentCloudKitContainer.name).sqlite-wal"))
-                            try? FileManager.default.removeItem(at: url.deletingLastPathComponent().appendingPathComponent("ckAssetFiles"))
-                        })
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-                    
-                    // delete old store
-                    let fileCoordinator = NSFileCoordinator(filePresenter: nil)
-                    fileCoordinator.coordinate(writingItemAt: oldStoreURL, options: .forDeleting, error: nil, byAccessor: { url in
-                        do {
-                            try FileManager.default.removeItem(at: oldStoreURL)
-                        } catch {
-                            print(error.localizedDescription)
-                        }
-                    })
-                }
-            }
-        }
-        print(persistentCloudKitContainer.persistentStoreDescriptions)
-        
-        // This is to make sure there is no duplicate bookmark or folder.
-        // Deduplicate bookmarks
-        var uniqueBookmarks: Set<UUID> = Set()
-        var bookmarksToDelete: [Bookmark] = []
-
-        do {
-            let bookmarks = try persistentCloudKitContainer.viewContext.fetch(Bookmark.fetchRequest())
-            bookmarks.forEach { bookmark in
-                if let id = bookmark.id {
-                    if !uniqueBookmarks.contains(id) {
-                        uniqueBookmarks.insert(id)
-                    } else {
-                        bookmarksToDelete.append(bookmark)
-                    }
-                }
-            }
-        } catch let error {
-            print("Couldn't fetch all bookmarks: \(error.localizedDescription)")
-        }
-
-        bookmarksToDelete.forEach { bookmark in
-            persistentCloudKitContainer.viewContext.delete(bookmark)
-        }
-
-        // Deduplicate folders
-        var uniqueFolders: Set<UUID> = Set()
-        var foldersToDelete: [Folder] = []
-
-        do {
-            let folders = try persistentCloudKitContainer.viewContext.fetch(Folder.fetchRequest())
-            folders.forEach { folder in
-                if let id = folder.id {
-                    if !uniqueFolders.contains(id) {
-                        uniqueFolders.insert(id)
-                    } else {
-                        foldersToDelete.append(folder)
-                    }
-                }
-            }
-        } catch let error {
-            print("Couldn't fetch all folders: \(error.localizedDescription)")
-        }
-
-        foldersToDelete.forEach { folder in
-            persistentCloudKitContainer.viewContext.delete(folder)
-        }
-
-        try? persistentCloudKitContainer.viewContext.save()
-
+        })
     }
     
-    func deduplicate<T: NSManagedObject>(type: T.Type, idKeyPath: KeyPath<T, UUID?>) {
+    /// To delete the previous persistent store, and migrate it to the new location. (from: https://stackoverflow.com/a/72585271)
+    /// - Parameters:
+    ///   - oldStoreURL: The previous Core Data Store Location
+    ///   - newStoreURL: The new Core Data Store Location
+    private func migrateStore(from oldStoreURL: URL, to newStoreURL: URL) {
+        let coordinator = persistentCloudKitContainer.persistentStoreCoordinator
+
+        guard let oldStore = coordinator.persistentStore(for: oldStoreURL) else {
+            return
+        }
+
+        do {
+            try coordinator.migratePersistentStore(oldStore, to: newStoreURL, options: nil, withType: NSSQLiteStoreType)
+
+            try coordinator.destroyPersistentStore(at: oldStoreURL, ofType: NSSQLiteStoreType, options: nil)
+            try FileManager.default.removeItem(at: oldStoreURL.deletingLastPathComponent().appendingPathComponent("\(persistentCloudKitContainer.name).sqlite-shm"))
+            try FileManager.default.removeItem(at: oldStoreURL.deletingLastPathComponent().appendingPathComponent("\(persistentCloudKitContainer.name).sqlite-wal"))
+            try FileManager.default.removeItem(at: oldStoreURL.deletingLastPathComponent().appendingPathComponent("ckAssetFiles"))
+            
+            let fileCoordinator = NSFileCoordinator(filePresenter: nil)
+            fileCoordinator.coordinate(writingItemAt: oldStoreURL, options: .forDeleting, error: nil, byAccessor: { url in
+                do {
+                    try FileManager.default.removeItem(at: oldStoreURL)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            })
+        } catch {
+            print("Migration failed: \(error)")
+        }
+    }
+
+    private func deduplicateBookmarksAndFolders() {
+        deduplicate(type: Bookmark.self, idKeyPath: \.id)
+        deduplicate(type: Folder.self, idKeyPath: \.id)
+    }
+
+    private func deduplicate<T: NSManagedObject>(type: T.Type, idKeyPath: KeyPath<T, UUID?>) {
         guard let entityName = T.entity().name else {
             return
         }
-        
+
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
         request.returnsObjectsAsFaults = false
-        
+
         do {
             let objects = try persistentCloudKitContainer.viewContext.fetch(request) as? [T] ?? []
             var uniqueIDs: Set<UUID> = Set()
             var objectsToDelete: [T] = []
-            
+
             for object in objects {
                 if let id = object[keyPath: idKeyPath] {
                     if uniqueIDs.contains(id) {
@@ -145,22 +102,25 @@ class DataController: ObservableObject {
                     }
                 }
             }
-            
+
             objectsToDelete.forEach { object in
                 persistentCloudKitContainer.viewContext.delete(object)
             }
-            
-            try? persistentCloudKitContainer.viewContext.save()
-        } catch let error {
-            print("Couldn't fetch \(entityName): \(error.localizedDescription)")
+
+            save()
+        } catch {
+            print("Couldn't fetch \(entityName): \(error)")
         }
     }
-    
-    func save(context: NSManagedObjectContext) {
+
+    func save() {
         do {
-            try context.save()
+            let context = persistentCloudKitContainer.viewContext
+            if context.hasChanges {
+                try context.save()
+            }
         } catch {
-            print("Error \(error.localizedDescription)")
+            print("Error \(error)")
         }
     }
 }
