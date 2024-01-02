17e2b7196bdb8093a747e5bcfcbca7084a27e2cf
Om Chachad <96822675+iTechEverything@users.noreply.github.com>
2024-01-02 16:58:18 +0530

    Added extension to allow conditional modifiers

--- Linkeeper/Helpers/Extensions/View.swift
+++ Linkeeper/Helpers/Extensions/View.swift
@@ -49,4 +49,17 @@ extension View {
         self
             .clipShape(RoundedRectangle(cornerRadius: radius, style: style))
     }
+    
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
 }
