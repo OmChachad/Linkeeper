6ec524ccf10d5228b22a968cc4e72ec41abc7534
Om Chachad <96822675+iTechEverything@users.noreply.github.com>
2024-01-05 10:04:11 +0530

    Share Extension Now Supports Mac Catalyst
    Added Share Extension for Mac Catalyst,
    
    The itemProvider on macOS returns a hexadecimal sequence in a value of type NSSecureCoding, whereas the iOS version simply returns a value that can be down-casted to URL. This is now taken care of, and the share extension works as expected on Mac Catalyst as well as iOS.

--- Share Extension/ShareViewController.swift
+++ Share Extension/ShareViewController.swift
@@ -18,21 +18,46 @@ class ShareViewController: UIViewController {
         guard let extensionContext = self.extensionContext else { return }
 
         if let inputItem = extensionContext.inputItems.first as? NSExtensionItem,
-           let itemProvider = inputItem.attachments?.first,
-           itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier) {
-            itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier, options: nil) { item, error in
-                if let url = item as? URL {
-                    DispatchQueue.main.async {
-                        let dataController = DataController.shared
-                        let swiftUIView = AddBookmarkView(urlString: url.absoluteString, onComplete: self.close).environment(\.managedObjectContext, dataController.persistentCloudKitContainer.viewContext)
-                        let hostingController = UIHostingController(rootView: swiftUIView)
-                        self.addChild(hostingController)
-                        self.view.addSubview(hostingController.view)
-                        hostingController.view.frame = self.view.bounds
-                        hostingController.didMove(toParent: self)
-                    }
-                }
+           let itemProvider = inputItem.attachments?.first {
+            if itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier) {
+                itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier,completionHandler: onLoadURL)
             }
+        }
+    }
+    
+    func onLoadURL(data: NSSecureCoding?, error: Error?) {
+        var urlString = ""
+        
+        #if targetEnvironment(macCatalyst)
+            let data = data as? Data
+            if let data, let absoluteString = String(data: data, encoding: .utf8) {
+                urlString = absoluteString
+            }
+        #else
+            if let url = data as? URL {
+                urlString = url.absoluteString
+            }
+        #endif
+        
+        DispatchQueue.main.async {
+            let dataController = DataController.shared
+            let managedObjectContext = dataController.persistentCloudKitContainer.viewContext
+            let swiftUIView = AddBookmarkView(urlString: urlString, onComplete: self.close)
+                .environment(\.managedObjectContext, managedObjectContext)
+                .navigationViewStyle(.stack)
+            
+            let hostingController = UIHostingController(rootView: swiftUIView)
+            self.addChild(hostingController)
+            self.view.addSubview(hostingController.view)
+            
+            #if targetEnvironment(macCatalyst)
+            hostingController.view.layer.cornerRadius = 10
+            hostingController.view.layer.cornerCurve = .continuous
+            hostingController.view.layer.masksToBounds = true
+            #endif
+            hostingController.view.frame = self.view.bounds
+            hostingController.didMove(toParent: self)
+            
         }
     }
     
@@ -44,4 +69,3 @@ class ShareViewController: UIViewController {
         super.viewDidAppear(animated)
     }
 }
-
