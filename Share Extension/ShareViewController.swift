e0f374caa9ac3934869d939e39f494ada45381e5
Om Chachad <omchachad12@gmail.com>
2025-03-01 10:29:31 +0530

    Added Support for YouTube and Other App URLs; Added Failure Alert

--- Share Extension/ShareViewController.swift
+++ Share Extension/ShareViewController.swift
@@ -21,33 +21,71 @@ class ShareViewController: UIViewController {
            let itemProvider = inputItem.attachments?.first {
             if itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier) {
                 itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier,completionHandler: onLoadURL)
+            } else if itemProvider.hasItemConformingToTypeIdentifier(UTType.text.identifier) {
+                itemProvider.loadItem(forTypeIdentifier: UTType.text.identifier, completionHandler: onLoadText)
+            } else {
+                presentErrorAlert()
             }
         }
     }
     
+    func onLoadText(data: NSSecureCoding?, error: Error?) {
+        if let text = data as? String {
+            // Use NSDataDetector to find URLs in the text
+            let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+            let matches = detector?.matches(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count))
+
+            if let firstMatch = matches?.first, let range = Range(firstMatch.range, in: text) {
+                let urlString = String(text[range])
+                if let url = URL(string: urlString) {
+                    presentAddBookmarkView(url: url)
+                    return
+                }
+            }
+        }
+        
+        presentErrorAlert()
+    }
+    
     func onLoadURL(data: NSSecureCoding?, error: Error?) {
         if let url = data as? URL {
-            DispatchQueue.main.async {
-                let dataController = DataController.shared
-                let managedObjectContext = dataController.persistentCloudKitContainer.viewContext
-                let swiftUIView = AddBookmarkView(urlString: url.absoluteString, onComplete: self.close)
-                    .environment(\.managedObjectContext, managedObjectContext)
-                    .navigationViewStyle(.stack)
-                
-                let hostingController = UIHostingController(rootView: swiftUIView)
-                self.addChild(hostingController)
-                self.view.addSubview(hostingController.view)
-                
-                hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-                hostingController.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-                hostingController.view.bottomAnchor.constraint (equalTo: self.view.bottomAnchor).isActive = true
-                hostingController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-                hostingController.view.rightAnchor.constraint (equalTo: self.view.rightAnchor).isActive = true
-
-                hostingController.didMove(toParent: self)
-            }
+            presentAddBookmarkView(url: url)
         } else {
-            self.close(false)
+            presentErrorAlert()
+        }
+    }
+    
+    func presentAddBookmarkView(url: URL) {
+        DispatchQueue.main.async {
+            let dataController = DataController.shared
+            let managedObjectContext = dataController.persistentCloudKitContainer.viewContext
+            let swiftUIView = AddBookmarkView(urlString: url.absoluteString, onComplete: self.close)
+                .environment(\.managedObjectContext, managedObjectContext)
+                .navigationViewStyle(.stack)
+            
+            let hostingController = UIHostingController(rootView: swiftUIView)
+            self.addChild(hostingController)
+            self.view.addSubview(hostingController.view)
+            
+            hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+            hostingController.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+            hostingController.view.bottomAnchor.constraint (equalTo: self.view.bottomAnchor).isActive = true
+            hostingController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+            hostingController.view.rightAnchor.constraint (equalTo: self.view.rightAnchor).isActive = true
+
+            hostingController.didMove(toParent: self)
+        }
+    }
+    
+    func presentErrorAlert() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "An error occurred",
+                                          message: "Could not extract link.",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+                self.close(false)
+            })
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
