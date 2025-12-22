86d39f001c416ea45dddf3753a0614e6469cf59b
Om Chachad <96822675+OmChachad@users.noreply.github.com>
2025-12-22 17:32:19 +0530

    Minor fixes for iOS Compilation

--- Linkeeper/Helpers/Extensions/View.swift
+++ Linkeeper/Helpers/Extensions/View.swift
@@ -132,7 +132,7 @@ extension View {
     
     @ViewBuilder
     func compatibleSafeAreaBar(edge: VerticalEdge, alignment: HorizontalAlignment = .center, spacing: CGFloat? = nil, supplyBackground: Bool = false, @ViewBuilder content: () -> some View) -> some View {
-        if #available(macOS 26.0, *) {
+        if #available(iOS 26.0, macOS 26.0, *) {
             self
                 .safeAreaBar(edge: edge, alignment: alignment, spacing: spacing, content: content)
         } else if supplyBackground {
