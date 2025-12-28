40e518acd8db8124138b1da06c6514848da6851d
Om Chachad <96822675+OmChachad@users.noreply.github.com>
2025-12-28 11:19:40 +0530

    Introduced Liquid Glass Icon Composer Icon

--- Linkeeper/BookmarkInfoViews/ChangeIconsView.swift
+++ Linkeeper/BookmarkInfoViews/ChangeIconsView.swift
@@ -10,38 +10,23 @@ import SwiftUI
 #if !os(macOS)
 @available(iOS 15.0, *)
 struct ChangeIconsView: View {
-    let icons = ["ClassicIcon", "DarkIcon"]
+    let icons = ["DarkIcon"]
     let displayNames = [
-        "ClassicIcon": "Classic",
         "DarkIcon": "Dark"
     ]
     
     @State private var initialised = false
-    @State private var chosenIcon = "AppIcon"
+    @State private var chosenIcon: String? = "AppIcon"
     @State private var showErrorAlert = false
     @State private var errorMessage = "An unknown error occured."
     
     var body: some View {
         Form {
             Picker("Choose an icon", selection: $chosenIcon) {
+                iconRow("ClassicIconImage", displayName: "Classic", tag: nil)
+                
                 ForEach(icons, id: \.self) { icon in
-                    HStack {
-                        Image("\(icon)Image")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 65, height: 65)
-                            #if os(visionOS)
-                            .clipShape(.circle)
-                            #else
-                            .cornerRadius(13, style: .continuous)
-                            #endif
-                            .padding([.trailing, .top, .bottom], 5)
-                        
-                        Text(displayNames[icon]!)
-                        
-                        Spacer()
-                    }
-                    .tag(icon)
+                    iconRow("\(icon)Image", displayName: displayNames[icon]!, tag: icon)
                 }
             }
             .pickerStyle(.inline)
@@ -55,14 +40,34 @@ struct ChangeIconsView: View {
                 initialised.toggle()
             }
         }
-        .onChange(of: chosenIcon) { newIcon in
-            setIcon(newIcon)
+        .onChange(of: chosenIcon ?? "") { _ in
+            setIcon(chosenIcon)
         }
         .navigationTitle("Change App Icon")
         .navigationBarTitleDisplayMode(.inline)
     }
     
-    func setIcon(_ iconName: String) {
+    func iconRow(_ iconName: String, displayName: String, tag: String?) -> some View {
+        HStack {
+            Image(iconName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 65, height: 65)
+                #if os(visionOS)
+                .clipShape(.circle)
+                #else
+                .cornerRadius(13, style: .continuous)
+                #endif
+                .padding([.trailing, .top, .bottom], 5)
+            
+            Text(displayName)
+            
+            Spacer()
+        }
+        .tag(tag)
+    }
+    
+    func setIcon(_ iconName: String?) {
         if initialised {
             UIApplication.shared.setAlternateIconName(iconName) { error in
                 if let error = error?.localizedDescription {
